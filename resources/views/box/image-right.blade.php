43965358d79ce861f9ac4b1332fd609fa34a9840
test4 <zellvid112@gmail.com>
2026-05-08 13:14:13 +0700

    update responsive screen phone

--- resources/views/box/image-right.blade.php
+++ resources/views/box/image-right.blade.php
@@ -1,20 +1,33 @@
-<div class="col-md-12 mt-3">
-    <img src="/left-img/displayitem_5dd99a71-9450-4f2e-a3ef-8df14c10696f.jpg" class="w-100 h-auto" alt="Special Offer">
-</div>
-<div class="col-md-12 mt-1">
-    <img src="/left-img/displayitem_8ad9b5e0-fd76-407b-b820-6494f03ffc311(1).jpg" class="w-100 h-auto"
-        alt="Special Offer">
-</div>
-<div class="col-md-12 mt-1">
-    <img src="/left-img/displayitem_92caf13f-61ae-4634-83c5-124d84633848.png" class="w-100 h-auto" alt="Special Offer">
-</div>
-<div class="col-md-12 mt-1">
-    <img src="/left-img/displayitem_ad84783d-4811-49c6-99e8-79a1085b0d8b.png" class="w-100 h-auto" alt="Special Offer">
-</div>
-<div class="col-md-12 mt-1">
-    <img src="/left-img/W1_PC.jpg" class="w-100 h-auto" alt="Special Offer">
-</div>
-<div class="col-md-12 mt-1">
-    <img src="/left-img/W1_PC2.jpg" class="w-100 h-auto" alt="Special Offer">
-</div>
+<div class="row">
 
+    <div class="img-wrapper">
+        <div class="img-right">
+            <img src="/left-img/displayitem_5dd99a71-9450-4f2e-a3ef-8df14c10696f.jpg">
+        </div>
+    </div>
+
+    <div class="img-wrapper">
+        <div class="img-right">
+            <img src="/left-img/displayitem_8ad9b5e0-fd76-407b-b820-6494f03ffc311(1).jpg">
+        </div>
+    </div>
+
+    <div class="img-wrapper">
+        <div class="img-right">
+            <img src="/left-img/displayitem_ad84783d-4811-49c6-99e8-79a1085b0d8b.png">
+        </div>
+    </div>
+
+    <div class="img-wrapper">
+        <div class="img-right">
+            <img src="/left-img/displayitem_eea963ab-9659-4fb7-87c4-36be5b82917a.png">
+        </div>
+    </div>
+
+    <div class="img-wrapper">
+        <div class="img-right">
+            <img src="/left-img/W1_PC.jpg">
+        </div>
+    </div>
+
+</div>
